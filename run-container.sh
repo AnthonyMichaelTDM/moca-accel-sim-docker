f0f5d9ca2566a691561d3619c3fa2c1c856e90b0
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-06-23 00:09:18 -0700

    chore: Update run-container.sh to support GPU acceleration if available

--- run-container.sh
+++ run-container.sh
@@ -8,4 +8,12 @@ if [ "$(docker image ls -q moca-accel-sim)" == "" ]; then
 else
     echo "Docker image already exists"
 fi
-docker run -it -v shared:/shared moca-accel-sim
+
+# if nvidia-container-toolkit is installed, run the container with GPU support
+if [ "$(command -v nvidia-container-toolkit)" != "" ]; then
+    echo "Running the container with GPU support"
+    docker run -it --gpus all -v shared:/shared moca-accel-sim
+else
+    echo "Running the container without GPU support"
+    docker run -it -v shared:/shared moca-accel-sim
+fi
