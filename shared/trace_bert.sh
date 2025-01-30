bebb86e4bf678c8c9db979fa992998476274842d
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2025-01-30 14:27:31 -0800

    misc

--- shared/trace_bert.sh
+++ shared/trace_bert.sh
@@ -6,8 +6,9 @@ export CUDA_VISIBLE_DEVICES=0
 export CUDA_VERSION="11.7"
 # export DYNAMIC_KERNEL_LIMIT_END="10"
 
-# export KERNEL_NAME_FILTER="_ZN2at6native44_GLOBAL__N__50c743a2_11_Indexing_cu_89862edb21indexSelectSmallIndexIfljLi2ELi2ELin2EEEvNS_4cuda6detail10TensorInfoIT_T1_EENS5_IKS6_S7_EENS5_IKT0_S7_EEiiS7_l"
-export DYNAMIC_KERNEL_REGIONS="1-2,4-4"
+# export KERNEL_NAME_FILTER="gemm"
+export SKIP_REPEAT_KERNEL_NAMES=1
+# export DYNAMIC_KERNEL_REGIONS="1-2,4-4"
 
 # export KERNEL_NAME_FILTER="_ZN7cutlass7Kernel2I50cutlass_80_tensorop_s1688gemm_64x64_16x6_tn_align4EEvNT_6ParamsE,_ZN2at6native29vectorized_elementwise_kernelILi4ENS0_13AUnaryFunctorIllbNS0_51_GLOBAL__N__28ce311f_18_CompareEQKernel_cu_d8008c9616CompareEqFunctorIlEEEENS_6detail5ArrayIPcLi2EEEEEviT0_T1_"
 # export DYNAMIC_KERNEL_REGIONS="4-38"
